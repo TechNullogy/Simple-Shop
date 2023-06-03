620e1dc3a15a049618cbcbc536076da068608f69
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2023-06-03 18:27:59 +0330

    feat: add box-shadow to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./*.html", "./src/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./*.html", "./src/**/*.html"],
+    theme: {
+        extend: {
+            boxShadow: {
+                "shadow-1": "0px 24px 40px rgba(121, 127, 136, 0.05)",
+            },
+        },
+    },
+    plugins: [],
 };
