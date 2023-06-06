fdd05f576bbbf232f5cbe365733adc070a6a9bee
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2023-06-06 10:33:24 +0330

    feat: add shadow-2 for recommended section of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
         extend: {
             boxShadow: {
                 "shadow-1": "0px 24px 40px rgba(121, 127, 136, 0.05)",
+                "shadow-2": "0px 14px 24px 10px #7E8AAB1C",
             },
         },
     },
