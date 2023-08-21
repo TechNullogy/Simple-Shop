9c054a309e43925ebebfb48b9901d1d5847902e6
Tahmineh Rivandi <tahmineh.r1992@gmail.com>
2023-08-21 20:03:44 +0330

    fix: change customize css classes to tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@
 export default {
   content: ["./*.html", "./src/**/*.html"],
   theme: {
-    extend: {
-      
+    colors:{
+      'blue': '#7E8AAB',
+      'light': '#F7F9FC',
+      'gray': '#7a7f8d',
     },
+    extend: {},
   },
   plugins: [],
 };
