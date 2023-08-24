f495ecbb2c4155ab7552f2e93f1bc50f42a839ea
Tahmineh Rivandi <tahmineh.r1992@gmail.com>
2023-08-24 18:40:18 +0330

    feat: customize colors in tailwindconf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@
 export default {
   content: ["./*.html", "./src/**/*.html"],
   theme: {
+   
     colors:{
       'blue': '#7E8AAB',
       'light': '#F7F9FC',
       'gray': '#7a7f8d',
       'white':'#ffff',
       'light-gray':'#F5F8FC',
+
     },
     extend: {},
   },
